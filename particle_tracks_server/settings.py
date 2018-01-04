cadfcd8b4d671dc86b2889fe3dfdc23796b4cb21
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-01-04 11:57:23 -0500

    Disable CORS; capture configuration information

--- particle_tracks_server/settings.py
+++ particle_tracks_server/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'particle_tracks_server.urls'
 # https://pypi.python.org/pypi/django-cors-middleware/1.3.1
 # https://github.com/ottoyiu/django-cors-headers/issues/102
 # need to refine this at some point...!
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
