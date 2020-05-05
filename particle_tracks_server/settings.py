8c044139a8b3a376ebe8e605e3bbd543f5a1ffa5
Ken Kiers <knkiers@taylor.edu>
2020-05-05 14:19:14 -0400

    update static setting

--- particle_tracks_server/settings.py
+++ particle_tracks_server/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-#STATIC_URL = 'https://particle-tracks.physics.taylor.edu/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = 'https://particle-tracks.physics.taylor.edu/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
